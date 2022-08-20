3863ea647c2e52c23b99e2f37da70f3db95598a4
Alinevictoriasouza <alynevictoriasouza47@gmail.com>
2022-08-20 13:29:31 -0300

    Site finalizado

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ converterDolar = () => {
   const resultado = dolar*5.09;
   document.getElementById('resultado').innerHTML =resultado
 }
+
+converterEuro = () => {
+  const euro = document.getElementById('euro').value;
+  const resultados = euro*5.19;
+  document.getElementById('resultados').innerHTML =resultados
+}
